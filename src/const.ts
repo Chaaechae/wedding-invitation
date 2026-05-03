03034453ddf4b80858ad7b06fffd8376ee50367c
Haesung Choi <gotjd715@gmail.com>
2026-05-03 16:41:43 +0900

    first commit

--- src/const.ts
+++ src/const.ts
@@ -9,15 +9,15 @@ dayjs.locale("ko")
 
 export { dayjs }
 
-export const WEDDING_DATE = dayjs.tz("2024-08-24 13:00", "Asia/Seoul")
+export const WEDDING_DATE = dayjs.tz("2026-09-12 13:00", "Asia/Seoul")
 export const WEDDING_DATE_FORMAT = `YYYY년 MMMM D일 dddd A h시${WEDDING_DATE.minute() === 0 ? "" : " m분"}`
 
 // 예식 당월 휴무일. 켈린더에 표시하기 위함.
 // 예: 예식일 8월 -> 8월 15일 광복절
 export const HOLIDAYS = [15]
 
-export const LOCATION = "서울대학교 연구공원 웨딩홀"
-export const LOCATION_ADDRESS = "서울시 관악구 관악로 1, 연구공원 본관 1층"
+export const LOCATION = "삼성전자 서초사옥 (5층 다목적홀)"
+export const LOCATION_ADDRESS = "서울특별시 서초구 서초대로74길 11"
 
 // 카카오톡 공유 시 위치 정보로 사용할 주소.
 // LOCATION 과 동일하게 설정해도 무방하나, 필요에 따라 좀 더 상세히 작성 가능.
@@ -37,37 +37,37 @@ export const NMAP_PLACE_ID = 13321741
 // 예: https://place.map.kakao.com/8634826 -> 8634826
 export const KMAP_PLACE_ID = 8634826
 
-export const BRIDE_FULLNAME = "정지원"
-export const BRIDE_FIRSTNAME = "지원"
-export const BRIDE_TITLE = "장녀"
-export const BRIDE_FATHER = "정상원"
-export const BRIDE_MOTHER = "박윤정"
+export const BRIDE_FULLNAME = "윤채원"
+export const BRIDE_FIRSTNAME = "채원"
+// export const BRIDE_TITLE = "장녀"
+export const BRIDE_FATHER = "윤재경"
+export const BRIDE_MOTHER = "공명아"
 export const BRIDE_INFO = [
   {
     relation: "신부",
     name: BRIDE_FULLNAME,
-    phone: "010-0000-0000",
-    account: "우리은행 0000000000000",
+    phone: "010-6335-6095",
+    account: "신한은행 110237552513",
   },
   {
     relation: "신부 아버지",
     name: BRIDE_FATHER,
-    phone: "010-0000-0000",
+    phone: "010-6284-9577",
     account: "하나은행 00000000000",
   },
   {
     relation: "신부 어머니",
     name: BRIDE_MOTHER,
-    phone: "010-0000-0000",
+    phone: "010-4780-6095",
     account: "하나은행 00000000000000",
   },
 ]
 
-export const GROOM_FULLNAME = "남주호"
-export const GROOM_FIRSTNAME = "주호"
-export const GROOM_TITLE = "차남"
-export const GROOM_FATHER = "남현태"
-export const GROOM_MOTHER = "김현영"
+export const GROOM_FULLNAME = "최해성"
+export const GROOM_FIRSTNAME = "해성"
+// export const GROOM_TITLE = "차남"
+export const GROOM_FATHER = "최교선"
+export const GROOM_MOTHER = "구지영"
 export const GROOM_INFO = [
   {
     relation: "신랑",
